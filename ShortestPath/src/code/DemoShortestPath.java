e686be9719602183e18cdedf9f9364619d2cd19b
Kin Wah Edward Lin <edward_lin@mymail.sutd.edu.sg>
2013-12-16 09:43:32 +0800

    - Amendment based on Exam Question - Import Data From File (Done)

--- ShortestPath/src/code/DemoShortestPath.java
+++ ShortestPath/src/code/DemoShortestPath.java
@@ -14,133 +14,97 @@ public class DemoShortestPath
 		if(args.length != 2 || args[0].equalsIgnoreCase("help"))
 		{
 			System.out.println("Argument is empty! Please provide the path of the input.txt");
-			System.out.println("DemoShortestPath Path/to/input.txt Path/to/output.txt");
+			System.out.println("DemoShortestPath Path/to/AdjList.txt Path/to/Schedule.txt");
 			return;
 		}
-		
-		// Output File Setting
-		FileOutputStream fos = new FileOutputStream(args[1]);
-		OutputStreamWriter out = new OutputStreamWriter(fos, "UTF-8");		
-		
-		/*
-		 *  Input File - Graph in Adjacency List
-		 *  Assume vertex's ID is int, if not, change it to int!
-		 *  NOTE: If ID is not int, then may need to implement the mapping (not likely, may ask to alter the input file!)
-		 *  
-		 *  So Far Assume the ID is {0, 1, 2, 3, .....}
-		 */
-		int i, j, numOfVertices;
+
+		// temp and loop variable
+		int i, j, numOfNeighbor, numOfState;
 		String line;
 		String[] strVertices = null;
-		Scanner scVertices = new Scanner(new File(args[0]));
-		Scanner scEdges = new Scanner(new File(args[0]));
+		String[] strSchedule = null;
 		
+		// Input Parameters
+		Scanner getAdjListNode = new Scanner(new File(args[0]));
+		Scanner getAdjListInfo = new Scanner(new File(args[0]));
+		Scanner getSchedule = new Scanner(new File(args[1]));
+				
 		// Graph Parameters
-		int srcID, destID;
 		ArrayList<Vertex> inputVertices = new ArrayList<Vertex>();
+		int numOfVertices;
 		
-		double adjacencyMatrix[][];
-		Graph myGraph;
-		
-		//------------------------------ Import Graph Info From Text File (Begin)--------------------------------------//
-		// Get the Vertices Info first, store as the first element in each line
-		while (scVertices.hasNextLine())
+		//------------------------------ Import Graph Info From AdjList (Begin)--------------------------------------//
+		// Get the Vertices ID first, store as the first element in each line
+		while (getAdjListNode.hasNextLine())
 		{
-			line = scVertices.nextLine();
+			line = getAdjListNode.nextLine();
 			
 			// <V-ID> <Adj-V-ID> <Weight?> <Adj-V-ID> <Weight?> .... 
 			strVertices = line.split("\\s+");
 
-			Vertex newVertex = new Vertex(Integer.parseInt(strVertices[0]), false);
+			Vertex newVertex = new Vertex(Integer.parseInt(strVertices[0]));
 			
 			inputVertices.add(newVertex);
 		}
-		scVertices.close();
+		getAdjListNode.close();
 		numOfVertices = inputVertices.size();
-		System.out.println("numOfVertices: " + numOfVertices);
-		
-		// Get the Edge Info, Create the Adjacency Matrix
-		adjacencyMatrix = new double[numOfVertices][];
-		for(i=0; i<numOfVertices; i++)
-			adjacencyMatrix[i] = new double[numOfVertices];
-		// Initialize the Adjacency Matrix, if i==j, set it to zero, other set it to Non-A-Number
-		for(i=0; i<numOfVertices; i++)
+		System.out.println("numOfVertices: " + numOfVertices);		
+
+		// Get the Vertices's neighbors
+		i=0;
+		while (getAdjListInfo.hasNextLine())
 		{
-			for(j=0; j<numOfVertices; j++)
-			{
-				if(i==j)
-					adjacencyMatrix[i][j] = 0;
-				else
-					adjacencyMatrix[i][j] = Double.NaN;
-			}
-		}
-						
-		j=0;
-		while (scEdges.hasNextLine())
-		{
-			line = scEdges.nextLine();
+			line = getAdjListInfo.nextLine();
 			
-			// <V-ID> <Adj-V-ID> <Weight?> <Adj-V-ID> <Weight?> .... 
+			// <V-ID> <Numer of Neighbor> <Adj-V-ID-1> <Adj-V-ID-2> ...
 			strVertices = line.split("\\s+");
-			
-			srcID = inputVertices.get(j).ID;
-			for(i=1; i<strVertices.length; i=i+2)
+
+			numOfNeighbor = Integer.parseInt(strVertices[1]);
+			System.out.println("ID:" + inputVertices.get(i).ID + "; numOfNeighbor: " + numOfNeighbor);
+			for(j=0; j<numOfNeighbor; j++)
 			{
-				// Vertex ID - Note May need to use name to find ID!
-				destID = Integer.parseInt(strVertices[i]);
-				
-				// Edge Weight
-				double weight = Double.parseDouble(strVertices[i+1]);
-				
-				adjacencyMatrix[srcID][destID] = weight;
+				inputVertices.get(i).neighbors.add(inputVertices.get(Integer.parseInt(strVertices[j+2])));
 			}
-			
-			j++;
-		}
-		scEdges.close();
-		myGraph = new Graph(inputVertices, adjacencyMatrix);
-		//------------------------------ Import Graph Info From Text File (End)--------------------------------------//
-		
-		//------------------------------ Print Adjacency Matrix (Begin)--------------------------------------//
-		myGraph.printAdjacencyMatrix();
-		//------------------------------ Print Adjacency Matrix (End)--------------------------------------//
-		
-		//------------------------------ Print Single Source Shortest Path (Begin)--------------------------------------//
-		srcID = 4;	
-		ArrayList<Vertex> resultShortPath = (ArrayList<Vertex>) myGraph.Dijkstra(myGraph.getVertex(srcID));
-		
-		System.out.println("From Source ID: " + srcID);
-		for(i=0; i<resultShortPath.size(); i++)
-		{
-			if(resultShortPath.get(i).parent != null)
-				System.out.println(resultShortPath.get(i).ID + " " + resultShortPath.get(i).key + " " + resultShortPath.get(i).parent.ID);
-			else
-				System.out.println(resultShortPath.get(i).ID + " " + resultShortPath.get(i).key);
-		}
-		//------------------------------ Print Single Source Shortest Path (End)--------------------------------------//
-		
-		//------------------------------ Print All-Pairs Shortest Paths (Begin)--------------------------------------//
-		double APSP[][] = myGraph.slowAPSP();
-		for(i=0; i<numOfVertices; i++)
-		{
-			for(j=0; j<numOfVertices; j++)
-				System.out.print(APSP[i][j] + " ");
+
+			System.out.print("its neighbor: ");
+			for(Vertex v:inputVertices.get(i).neighbors)
+				System.out.print(v.ID + " ");
 			System.out.print("\n");
+			
+			i++;
 		}
-		//------------------------------ Print All-Pairs Shortest Paths (End)--------------------------------------//
+		getAdjListInfo.close();
+
+
+		// Get the Vertex's Schedule
+		i=0;
+		while (getSchedule.hasNextLine())
+		{
+			line = getSchedule.nextLine();
+			
+			// <V-ID> <Numer of States> <time-1> <time-2> ...
+			strSchedule = line.split("\\s+");
+
+			numOfState = Integer.parseInt(strSchedule[1]);
+			System.out.println("ID:" + inputVertices.get(i).ID + "; numOfState: " + numOfState);
+			for(j=0; j<numOfState; j++)
+			{
+				inputVertices.get(i).schedule.add(Integer.parseInt(strSchedule[j+2]));
+			}
+
+			System.out.print("its schedule: ");
+			for(int v:inputVertices.get(i).schedule)
+				System.out.print(v + " ");
+			System.out.print("\n");
+			
+			i++;
+		}
+		getSchedule.close();		
+		//------------------------------ Import Graph Info From AdjList (End)--------------------------------------//
+	
+		//------------------------------ Calculate Optimal Solution (Begin)--------------------------------------//
 		
-		//------------------------------ Print Single Source Destination Longest Path (Begin)--------------------------------------//
-//		myGraph.Longest_Path_Main(myGraph.getVertex(1), myGraph.getVertex(6));
-//		myGraph.printLongest_Path(myGraph.getVertex(1), myGraph.getVertex(6));
-		//------------------------------ Print Single Source Destination Longest Path (End)--------------------------------------//
 		
-		//------------------------------ Print Single Source Destination Longest Path (Begin)--------------------------------------//
-		ArrayList<Vertex> sortedList = (ArrayList<Vertex>) myGraph.Topological_Sort_DAT();
-		for(Vertex v: sortedList)
-			System.out.println(v.ID + " " + v.key + " " + v.finishTime);
-		System.out.print("\n");
-		//------------------------------ Print Single Source Destination Longest Path (Begin)--------------------------------------//
-		
-		out.close();
+		//------------------------------ Calculate Optimal Solution (End)--------------------------------------//		
 	}
 }
--- ShortestPath/src/code/Vertex.java
+++ ShortestPath/src/code/Vertex.java
@@ -1,51 +1,19 @@
 package code;
 
+import java.util.ArrayList;
+
 public class Vertex 
 {
 	int ID;
-	String Name;
 	
-	double key;			// Based on the algorithm for comparison, e.g. DFS the search time
-	Vertex parent;		// Previous Vertex
-	Vertex next;		// Next Vertex
+	ArrayList<Vertex> neighbors;
+	ArrayList<Integer> schedule;
 	
-	double finishTime;	// For DFS 
-	Color color;		// For DFS
-	
-	Vertex()
-	{}
-	
-	Vertex(int ID, boolean isPositiveInfinity)
+	Vertex(int ID)
 	{
 		this.ID = ID;
 		
-		if(isPositiveInfinity)
-			key = Double.POSITIVE_INFINITY;
-		else
-			key = Double.NEGATIVE_INFINITY;
-		
-		parent = null;
-	}
-	
-	boolean isEqual(int ID)
-	{
-		if(this.ID == ID)
-			return true;
-		else
-			return false;
-	}
-	boolean isEqual(Vertex v)
-	{
-		if(this.ID == v.ID)
-			return true;
-		else
-			return false;		
-	}
-	
-	void copy(Vertex from)
-	{
-		this.ID = from.ID;
-		this.key = from.key;
-		this.parent = from.parent;
+		neighbors = new ArrayList<Vertex>();
+		schedule = new ArrayList<Integer>();
 	}
 }
